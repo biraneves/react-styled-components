3f297a9f04e3221fee4841bcef120cc2a969970c
Ubirajara Neves <ubirajara.neves@usp.br>
2022-09-07 13:25:13 -0300

    Tema com Styled Components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,95 @@
-import styled from 'styled-components';
+import { useState } from 'react';
+import styled, { css, ThemeProvider } from 'styled-components';
+
+type TStyledButtonProps = {
+    variant?: 'success' | 'fail';
+};
+
+// const RotateKeyFrame = keyframes`
+//     from {
+//         transform: rotate(0deg)
+//     }
+//     to {
+//         transform: rotate(360deg);
+//     }
+// `;
 
 const StyledHeader = styled.h1`
-    color: #3636e9;
+    color: ${(props) => props.theme.main};
     font-size: 42px;
 `;
 
 const StyledData = styled.p`
-    color: #8d8dff;
+    color: ${(props) => props.theme.secondary};
     font-size: 24px;
     font-weight: 700;
 `;
 
-const StyledButton = styled.button`
+const StyledButton = styled.button<TStyledButtonProps>`
     background-color: transparent;
     border-radius: 8px;
     border: 2px solid black;
     padding: 8px 16px;
+
+    :hover {
+        cursor: pointer;
+        background-color: #f2f2f2;
+    }
+
+    ${(props) => {
+        switch (props.variant) {
+            case 'success':
+                return css`
+                    border-color: green;
+                    color: green;
+                `;
+            case 'fail':
+                return css`
+                    border-color: red;
+                    color: red;
+                `;
+        }
+    }}
 `;
 
-const StyledSuccessButton = styled(StyledButton)`
-    border-color: green;
-    color: green;
-`;
+const themeBlue = {
+    main: 'blue',
+    secondary: '#8d8dff',
+};
 
-const StyledFailedButton = styled(StyledButton)`
-    border-color: red;
-    color: red;
-`;
+const themeRed = {
+    main: 'red',
+    secondary: '#fc7979',
+};
 
 function App() {
+    const [theme, setTheme] = useState(themeBlue);
+
     return (
         <div className="App">
-            <StyledHeader>Bira Neves</StyledHeader>
-            <StyledData>ubirajara.neves@usp.br</StyledData>
-            <StyledData>+55 11 555-1234</StyledData>
-            <StyledData>Brasil</StyledData>
+            <ThemeProvider theme={theme}>
+                <StyledHeader>Bira Neves</StyledHeader>
+                <div>
+                    <StyledButton
+                        onClick={() => {
+                            theme === themeBlue
+                                ? setTheme(themeRed)
+                                : setTheme(themeBlue);
+                        }}
+                    >
+                        Toggle Theme
+                    </StyledButton>
+                </div>
+                <StyledData>ubirajara.neves@usp.br</StyledData>
+                <StyledData>+55 11 555-1234</StyledData>
+                <StyledData>Brasil</StyledData>
 
-            <StyledSuccessButton>Adicionar</StyledSuccessButton>
-            <StyledFailedButton>Remover</StyledFailedButton>
-            <StyledButton>Detalhes</StyledButton>
+                <StyledButton variant="success">Adicionar</StyledButton>
+                <StyledButton variant="fail" style={{ margin: '0 8px' }}>
+                    Remover
+                </StyledButton>
+                <StyledButton>Detalhes</StyledButton>
+            </ThemeProvider>
         </div>
     );
 }
